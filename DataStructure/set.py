4713b808d7ab0cd7390e000017581abe26ce442c
king0jin <et0709@naver.com>
2024-12-07 14:30:25 +0900

    set 자료형

--- DataStructure/set.py
+++ DataStructure/set.py
@@ -30,3 +30,18 @@ print(my_set)  # 출력: {1, 2, 3}
 my_set = set()
 print(my_set)
 print(type(my_set)) 
+
+# set주요 메서드
+# add(): 새로운 요소를 추가.
+# remove(): 특정 요소를 제거 (존재하지 않으면 오류).
+# discard(): 특정 요소 제거 (존재하지 않아도 오류 없음).
+# 합집합: set1 | set2 또는 set1.union(set2)
+# 교집합: set1 & set2 또는 set1.intersection(set2)
+# 차집합: set1 - set2 또는 set1.difference(set2)
+# 대칭 차집합: set1 ^ set2 또는 set1.symmetric_difference(set2)
+# 부분 집합 여부: set1 <= set2 또는 set1.issubset(set2)
+# 상위 집합 여부: set1 >= set2 또는 set1.issuperset(set2)
+# 서로소 여부: set1.isdisjoint(set2)
+# len(set): 집합의 요소 개수.
+# in: 요소 존재 여부 확인.
+# clear(): 모든 요소 제거.
